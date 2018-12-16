7d831d0b3323a54fe02d67fcabc178bded28e79f
Avi <akvadali@gmail.com>
2018-12-16 17:38:50 -0600

    Update Problem48.java

--- Problem48/src/Problem48.java
+++ Problem48/src/Problem48.java
@@ -1,16 +1,21 @@
+/*
+The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+
+Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+*/
 import java.util.*;
 import java.math.BigInteger;
 public class Problem48 {
 public static void main(String[] args) {
-        BigInteger num = new BigInteger("0");
-        BigInteger temp = new BigInteger("1");
+        BigInteger n = new BigInteger("0");
+        BigInteger t = new BigInteger("1");
         BigInteger series = new BigInteger("0");
         for(int i = 1; i < 1001; i++) {
-                series = temp.pow(i);
-                temp = temp.add(BigInteger.ONE);
-                num = num.add(series);
+                series = t.pow(i);
+                t = t.add(BigInteger.ONE);
+                n = n.add(series);
 
         }
-        System.out.println(num);
+        System.out.println(n);
 }
 }
