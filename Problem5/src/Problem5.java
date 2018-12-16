81a4c7051bbd7cfd7eb81d7914fae6b1dc316592
Avi <akvadali@gmail.com>
2018-12-16 17:42:01 -0600

    Update Problem5.java

--- Problem5/src/Problem5.java
+++ Problem5/src/Problem5.java
@@ -1,9 +1,13 @@
+/*
+The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
 
+Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+*/
 public class Problem5 {
 
 final static int limit = 20;
 
-static int gcd(int x, int y) {
+static int lcm(int x, int y) {
 	while (y != 0) {
 		int z = x;
 		x = y;
@@ -15,8 +19,8 @@ static int gcd(int x, int y) {
 public static void main(String[] args) {
 	int answer = 1;
 	for (int i = 1; i <= limit; i++) {
-		int currentGcd = gcd(i, answer);
-		answer = answer / currentGcd * i;
+		int currentlcm = lcm(i, answer);
+		answer = answer / currentlcm * i;
 	}
 	System.out.println(answer);
 }
